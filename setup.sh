bf0d16e82d0bf6f886d6de4e6677670039b31b9f
Iain McNulty <iain@inulty.com>
2021-10-29 21:32:02 +0100

    [setup.sh] Add install commands for Brave but no default install

--- setup.sh
+++ setup.sh
@@ -101,6 +101,7 @@ fi
 
 # curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 # sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com stable main" > /etc/apt/sources.list.d/brave.list'
+# sudo apt-get update && sudo apt-get install brave-browser
 
 # # FZF
 if ! hash fzf &> /dev/null; then
