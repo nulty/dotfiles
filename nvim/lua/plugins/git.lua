88e1235bdf20d2039eb0a31509fba2390e1ecc73
Iain McNulty <iain@inulty.com>
2023-10-20 23:39:49 +0100

    jump hunks with gitsigns c] c[

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,23 +1,28 @@
 return {
   -- https://github.com/tpope/vim-fugitive
-	{
-		"tpope/vim-fugitive",
-		lazy = false,
-		keys = {
-			-- {
-			-- 	"<leader>gg",
-			-- 	vim.api.nvim_command(":G"),
-			-- 	desc = "Open Fugitive"
-			-- }
-		},
-    config = function ()
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+    config = function()
       vim.keymap.set('n', '<leader>gg', ":G<CR>")
     end
-	},
+  },
   -- https://github.com/lewis6991/gitsigns.nvim
   {
     'lewis6991/gitsigns.nvim',
     event = "BufEnter",
-    config = true
+    opts = {
+      on_attach = function(bufnr)
+        vim.keymap.set('n', ']c', '<CMD> lua require"gitsigns".next_hunk()<CR>', { buffer = bufnr, noremap = true })
+        vim.keymap.set('n', '[c', '<CMD> lua require"gitsigns".prev_hunk()<CR>', { buffer = bufnr, noremap = true })
+      end
+    },
+    config = function(config)
+      local setup = {
+        on_attach = config.opts.on_attach,
+      }
+
+      require('gitsigns').setup(setup)
+    end
   },
 }
