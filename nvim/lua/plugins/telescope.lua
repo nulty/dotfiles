2282eb3c5cf10b9d16e090ee678808bb89bea101
Iain McNulty <iain@inulty.com>
2024-01-22 06:48:56 +0000

    Add dir-telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    lazyb= true,
     event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim"
@@ -11,23 +10,37 @@ return {
       { "<leader>b", ":Telescope buffers<cr>" },
       { "<leader>m", ":Telescope keymaps<cr>" },
       { "<leader>f", ":Telescope live_grep<cr>" },
-      { "<leader>f", ":Telescope live_grep<cr>" },
+      { "<leader>g", ":Telescope dir live_grep<cr>" },
     },
-    config = function(config)
-      local t = require('telescope.actions').delete_buffer
-
+    config = function()
       require('telescope').setup({
         defaults = {
           mappings = {
             n = {
-                  ['<c-d>'] = require('telescope.actions').delete_buffer
+              ['<c-d>'] = require('telescope.actions').delete_buffer
             },
             i = {
-                  ['<c-d>'] = require('telescope.actions').delete_buffer
+              ['<c-d>'] = require('telescope.actions').delete_buffer
             }
           }
         }
       })
+    end
+  },
+  {
+    "princejoogie/dir-telescope.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("dir-telescope").setup({
+        -- these are the default options set
+        hidden = true,
+        no_ignore = true,
+        show_preview = true,
+      })
+      require("telescope").load_extension("dir")
     end
   }
 }
