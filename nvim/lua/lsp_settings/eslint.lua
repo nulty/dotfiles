0020c8aea86596657f2d84b704299a7d4dc53869
Iain McNulty <iain@inulty.com>
2024-03-10 22:06:29 +0000

    Add eslint lsp config

---
+++ nvim/lua/lsp_settings/eslint.lua
@@ -0,0 +1,29 @@
+return {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+  root_dir = function(path)
+    local eslint = vim.fs.find({ 'eslintrc.js', 'eslint.config.js' }, {
+      upward = true,
+      stop = vim.loop.os_homedir(),
+      path = vim.fs.dirname(path),
+    })
+
+    vim.print(vim.fs.dirname(eslint[1]))
+
+    return vim.fs.dirname(eslint[1])
+  end,
+  settings = {
+    debug = true,
+    experimental = {
+      useFlatConfig = true
+    },
+    -- runtime = "node",
+    -- lintTask = {
+    --   options = "-c eslint.config.js"
+    -- }
+  }
+}
