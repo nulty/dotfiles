45f217dc5d711129c194280f113a5b9d1608c829
Iain McNulty <iain@inulty.com>
2025-07-19 17:53:15 +0100

    Update eslint

--- nvim/lua/lsp_settings/eslint.lua
+++ nvim/lua/lsp_settings/eslint.lua
@@ -1,29 +1,32 @@
 return {
-  on_attach = function(client, bufnr)
+  on_attach = function(_client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
-      command = "EslintFixAll",
+      callback = function()
+        vim.lsp.buf.format({
+          filter = function(c)
+            return c.name == "eslint"
+          end,
+          async = false,
+        })
+      end,
     })
   end,
-  root_dir = function(path)
-    local eslint = vim.fs.find({ 'eslintrc.js', 'eslint.config.js' }, {
-      upward = true,
-      stop = vim.loop.os_homedir(),
-      path = vim.fs.dirname(path),
-    })
-
-    vim.print(vim.fs.dirname(eslint[1]))
-
-    return vim.fs.dirname(eslint[1])
-  end,
+  -- root_dir = function(path)
+  -- vim.print(path)
+  -- local eslint = vim.fs.find({ 'eslintrc.js', 'eslint.config.js' }, {
+  --   upward = true,
+  --   stop = vim.loop.os_homedir(),
+  --   path = vim.fs.dirname(path),
+  -- })
+  --
+  -- vim.print(vim.fs.dirname(eslint[1]))
+  --
+  -- return vim.fs.dirname(eslint[1])
+  -- end,
   settings = {
     debug = true,
-    experimental = {
-      useFlatConfig = true
-    },
+    useFlatConfig = true,
     -- runtime = "node",
-    -- lintTask = {
-    --   options = "-c eslint.config.js"
-    -- }
   }
 }
